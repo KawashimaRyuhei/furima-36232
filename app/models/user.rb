016b918b78dc18aa33c5a2a0641c80f5bec26ccf
KawashimaRyohei <11011011wxy@gmail.com>
2021-07-30 18:02:04 +0900

    コードレビュー後の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,23 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname
-    validates :encrypted_password, :password, :password_confirmation, length: { minimum: 6 },
-                                                                      format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/, message: 'Please include both letters and numbers in your settings' }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' }
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' }
-    validates :kana_first_name, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' }
-    validates :kana_last_name, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' }
+    
+    with_options length: { minimum: 6 },format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/, message: 'Please include both letters and numbers in your settings' } do
+      validates :encrypted_password
+      validates :password
+      validates :password_confirmation
+    end                                                               
+    
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' } do
+      validates :first_name 
+      validates :last_name
+    end
+    
+    with_options format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' } do
+      validates :kana_first_name
+      validates :kana_last_name
+    end
+    
     validates :birthday
   end
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -31,7 +31,7 @@ RSpec.describe User, type: :model do
     end
 
     it 'emailは@がないと登録できない' do
-      @user.email = '@aaa'
+      @user.email = 'aaa'
       @user.valid?
       expect(@user.errors.full_messages).to include('Email is invalid')
     end
