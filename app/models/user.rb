c5050d4b5d0630f7e6c1e1d5d005d53307e60242
KawashimaRyohei <11011011wxy@gmail.com>
2021-07-30 15:22:24 +0900

    Userモデルのテスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,11 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname
-    validates :first_name
-    validates :last_name
-    validates :kana_first_name
-    validates :kana_last_name
+    validates :encrypted_password,:password,:password_confirmation,length:{minimum:6},format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/, message: "Please include both letters and numbers in your settings"}
+    validates :first_name, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
+    validates :last_name, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
+    validates :kana_first_name, format: {with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters"}
+    validates :kana_last_name, format: {with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters"}
     validates :birthday
   end
 
