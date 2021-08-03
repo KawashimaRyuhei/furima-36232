7c88f0fe1308769bfb18c73e09dd3b81b219de63
KawashimaRyohei <11011011wxy@gmail.com>
2021-08-03 13:09:46 +0900

    不要なコメントアウトの削除、itemモデルのテスト追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -97,6 +97,12 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Price is not a number")
       end
 
+      it '販売価格が半角英語だけでは登録できない'do
+        @item.price = 'aaa'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+
       it 'Userが紐づいていないと出品できない'do
         @item.user = nil
         @item.valid?
