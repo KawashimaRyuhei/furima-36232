7545566ce295b083685b4089afdce7f334014273
KawashimaRyohei <11011011wxy@gmail.com>
2021-08-04 14:25:38 +0900

    条件式の変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -45,7 +45,7 @@ class ItemsController < ApplicationController
 
   def move_to_index
     @item = Item.find(params[:id])
-    if current_user != @item.user
+    unless current_user == @item.user
       redirect_to root_path
     end
   end
