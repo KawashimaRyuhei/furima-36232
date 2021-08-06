945e35dba6cb38368b9481f2cff66d16b70cf9fc
KawashimaRyohei <11011011wxy@gmail.com>
2021-08-06 21:29:43 +0900

    buy_sendモデルの編集

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,4 +1,26 @@
 class BuysController < ApplicationController
   def index
+    @buy = Item.find(params[:item_id])
+    @buy_send = BuySend.new
+  end
+
+  def new
+  end
+
+  def create
+    @buy_send = BuySend.new(buy_params)
+    if @buy_send.valid?
+      @buy_send.save
+      redirect_to root_path
+    else
+      @buy = Item.find(params[:item_id])
+      render :index
+    end
+  end
+    
+  private
+
+  def buy_params
+    params.require(:buy_send).permit(:postal_code, :prefectures_id, :city, :address, :building_name, :telephone).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
---
+++ app/models/buy_send.rb
@@ -0,0 +1,17 @@
+class BuySend
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefectures_id, :city, :address, :building_name, :telephone, :user_id, :item_id, :buy_id
+
+  with_options presence: true do
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :user_id, :item_id
+    validates :city, :address
+    validates :telephone, format: {with: /\A^(0{1}\d{9,10})$\z/, message: "is invalid. Remove hyphen(-)"}
+  end
+  validates :prefectures_id, numericality: {other_than: 0, message: "can't be blank"}
+
+  def save
+    buy = Buy.create(user_id: user_id, item_id: item_id)
+    Send.create(postal_code: postal_code, prefectures_id: prefectures_id, city: city, address: address, building_name: building_name, telephone: telephone, buy_id: buy.id)
+  end
+end
