4f174605a17e44b7ad0bf170b6bd5c39d960265c
KawashimaRyohei <11011011wxy@gmail.com>
2021-08-06 22:44:12 +0900

    ログインの有無、商品売却済みか否かで表示が変わるように変更

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,6 +1,10 @@
 class BuysController < ApplicationController
+  before_action :authenticate_user! 
+  before_action :find_params, only: [:index, :new, :create]
+  before_action :move_to_index
+
+  
   def index
-    @buy = Item.find(params[:item_id])
     @buy_transmit = BuyTransmit.new
   end
 
@@ -13,14 +17,30 @@ class BuysController < ApplicationController
       @buy_transmit.save
       redirect_to root_path
     else
-      @buy = Item.find(params[:item_id])
       render :index
     end
   end
     
   private
 
+
+  def find_params
+    @buy = Item.find(params[:item_id])
+  end
+
+  def move_to_index
+    if params[:item_id].blank? || current_user == @buy.user
+      redirect_to root_path
+    end
+  end
+
   def buy_params
     params.require(:buy_transmit).permit(:postal_code, :prefectures_id, :city, :address, :building_name, :telephone).merge(user_id: current_user.id, item_id: params[:item_id])
   end
+
+  
+
+  
+
+
 end
